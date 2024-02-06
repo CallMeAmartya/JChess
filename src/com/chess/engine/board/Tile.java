e4d808df0889ba487fda014fbcd24518ad2a51d9
Amartya Sen <amartyasen00077@gmail.com>
2024-02-06 21:09:03 +0530

    Immutability enhancements to Tile class

--- src/com/chess/engine/board/Tile.java
+++ src/com/chess/engine/board/Tile.java
@@ -1,53 +1,73 @@
 package com.chess.engine.board;
 
 import com.chess.engine.pieces.Piece;
+import com.google.common.collect.ImmutableMap;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public abstract class Tile {
 
-    int tileCoordinate;
+  protected final int tileCoordinate;
 
-    public Tile(int tileCoordinate) {
-        this.tileCoordinate = tileCoordinate;
+  private static final Map<Integer, EmptyTile> EMPTY_TILE_CACHE = createAllPossibleEmptyTiles();
+
+  private Tile(int tileCoordinate) {
+    this.tileCoordinate = tileCoordinate;
+  }
+
+  public static Tile createTile(final int tileCoordinate, final Piece pieceOnTile) {
+    return pieceOnTile == null
+        ? EMPTY_TILE_CACHE.get(tileCoordinate)
+        : new OccupiedTile(tileCoordinate, pieceOnTile);
+  }
+
+  public abstract boolean isTileOccupied();
+
+  public abstract Piece getPiece();
+
+  private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
+    Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
+    for (int i = 0; i < 64; i++) {
+      emptyTileMap.put(i, new EmptyTile(i));
+    }
+    return ImmutableMap.copyOf(emptyTileMap);
+  }
+
+  public static final class EmptyTile extends Tile {
+
+    public EmptyTile(final int tileCoordinate) {
+      super(tileCoordinate);
     }
 
-    public abstract boolean isTileOccupied();
-
-    public abstract Piece getPiece();
-
-    public static final class EmptyTile extends Tile {
-
-        public EmptyTile(int tileCoordinate) {
-            super(tileCoordinate);
-        }
-
-        @Override
-        public boolean isTileOccupied() {
-            return false;
-        }
-
-        @Override
-        public Piece getPiece() {
-            return null;
-        }
+    @Override
+    public boolean isTileOccupied() {
+      return false;
     }
 
-    public static final class OccupiedTile extends Tile {
-
-        Piece pieceOnTile;
-
-        public OccupiedTile(int tileCoordinate, Piece pieceOnTile) {
-            super(tileCoordinate);
-            this.pieceOnTile = pieceOnTile;
-        }
-
-        @Override
-        public boolean isTileOccupied() {
-            return true;
-        }
-
-        @Override
-        public Piece getPiece() {
-            return this.pieceOnTile;
-        }
+    @Override
+    public Piece getPiece() {
+      return null;
     }
+  }
+
+  public static final class OccupiedTile extends Tile {
+
+    private final Piece pieceOnTile;
+
+    public OccupiedTile(int tileCoordinate, Piece pieceOnTile) {
+      super(tileCoordinate);
+      this.pieceOnTile = pieceOnTile;
+    }
+
+    @Override
+    public boolean isTileOccupied() {
+      return true;
+    }
+
+    @Override
+    public Piece getPiece() {
+      return this.pieceOnTile;
+    }
+  }
 }
